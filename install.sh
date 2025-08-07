ebe7c7b8d287b1c56f124fb8e2a5c385a46dbbc8
CompVR <lemon.caruso@gmail.com>
2025-08-07 19:17:21 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -e
+
+######################################################################################
+#                                                                                    #
+# Project 'nova-installer'                                                           #
+#                                                                                    #
+# Copyright (C) 2025, Your Name or Company                                           #
+#                                                                                    #
+# This program is free software: you can redistribute it and/or modify              #
+# it under the terms of the GNU General Public License as published by              #
+# the Free Software Foundation, either version 3 of the License, or                 #
+# (at your option) any later version.                                               #
+#                                                                                    #
+# This program is distributed in the hope that it will be useful,                   #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                    #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                              #
+#                                                                                    #
+# https://github.com/yourusername/nova-installer                                     #
+#                                                                                    #
+# This script is not associated with the official Pterodactyl Project.              #
+#                                                                                    #
+######################################################################################
+
+export GITHUB_SOURCE="v1.1.1"
+export SCRIPT_RELEASE="v1.1.1"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/Emil7YT/Nova-Panel"
+
+LOG_PATH="/var/log/nova-installer.log"
+
+# Check for curl
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+# Always remove lib.sh, before downloading it
+[ -f /tmp/lib.sh ] && rm -rf /tmp/lib.sh
+curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/master/lib/lib.sh
+# shellcheck source=lib/lib.sh
+source /tmp/lib.sh
+
+execute() {
+  echo -e "\n\n* nova-installer $(date) \n\n" >>$LOG_PATH
+
+  [[ "$1" == *"canary"* ]] && export GITHUB_SOURCE="master" && export SCRIPT_RELEASE="canary"
+  update_lib_source
+  run_ui "${1//_canary/}" |& tee -a $LOG_PATH
+
+  if [[ -n $2 ]]; then
+    echo -e -n "* Installation of $1 completed. Do you want to proceed to $2 installation? (y/N): "
+    read -r CONFIRM
+    if [[ "$CONFIRM" =~ [Yy] ]]; then
+      execute "$2"
+    else
+      error "Installation of $2 aborted."
+      exit 1
+    fi
+  fi
+}
+
+welcome ""
+
+done=false
+while [ "$done" == false ]; do
+  options=(
+    "Install the Nova Panel"
+    "Install Nova Wings"
+    "Install both [0] and [1] on the same machine"
+
+    "Install Nova Panel (canary version)"
+    "Install Nova Wings (canary version)"
+    "Install both [3] and [4] on the same machine"
+    "Uninstall panel or wings (canary version)"
+  )
+
+  actions=(
+    "panel"
+    "wings"
+    "panel;wings"
+
+    "panel_canary"
+    "wings_canary"
+    "panel_canary;wings_canary"
+    "uninstall_canary"
+  )
+
+  output "What would you like to do?"
+
+  for i in "${!options[@]}"; do
+    output "[$i] ${options[$i]}"
+  done
+
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+
+  [ -z "$action" ] && error "Input is required" && continue
+
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
+done
+
+rm -rf /tmp/lib.sh
